05c58d096a1948830da257628a955757f5d1c76e
NSSKG-dev <noskillsnipe18@gmail.com>
2026-08-24 16:17:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2678,12 +2678,6 @@ def health():
 
 if __name__ == "__main__":
     init_db()
-    print("=" * 55)
-    print("DISASTER X")
-    print("http://127.0.0.1:5000")
-    print("=" * 55)
-if __name__ == "__main__":
-    init_db()
 
     port = int(os.environ.get("PORT", 5000))
 
